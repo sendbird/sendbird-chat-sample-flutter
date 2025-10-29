99b01e69d2351b0c71b5008ed09ca09a107270fc
Tyler Jeong <tyler.jeong@sendbird.com>
2025-10-29 14:21:06 +0900

    Update.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,7 +8,7 @@ import Foundation
 import connectivity_plus
 import file_picker
 import flutter_local_notifications
-import isar_flutter_libs
+import isar_community_flutter_libs
 import path_provider_foundation
 import shared_preferences_foundation
 
